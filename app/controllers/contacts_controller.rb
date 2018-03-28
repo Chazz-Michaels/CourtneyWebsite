f0bcdd99f31b7e1a1c82d17593d1e42e556dcc86
Mike Chase <mikec17249@gmail.com>
2018-03-27 20:03:25 -0400

    fix

--- app/controllers/contacts_controller.rb
+++ app/controllers/contacts_controller.rb
@@ -2,15 +2,24 @@ class ContactsController < ApplicationController
   # encoding: utf-8
   def new
     @contact = Contact.new
+    if
+      Contact.new(params[:contact]).deliver
+      redirect_to '/contacts/new', :alert => ["Yeah!"]
+    end
+  else
+
   end
+
 
   def create
     @contact = Contact.new(params[:contact])
-    @contact.request = request
-    if @contact.deliver
-      flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
+    if
+    @contact.valid?
+      @contact.deliver
+    end
+      #flash.now[:notice] = 'Thank you for your message. We will contact you soon!'
     else
-      flash.now[:error] = 'Cannot send message.'
+      #flash.now[:error] = 'Cannot send message.'
       render :new
     end
   end
